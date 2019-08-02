18ff539ad617b51aa52f7423939ceeaca93a7c6b
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-01 21:42:46 -0400

    create destination and category controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+  def show
+    render json: @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      render json: @category
+    else
+      render json: {error: 'Unable to create a new category.'}
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: {error: 'Unable to update the category.'}
+    end
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,8 +1,43 @@
 class Api::V1::DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :update, :destroy]
 
   def index
     @destinations = Destination.all
     render json: @destinations
   end
-  
+
+  def show
+    render json: @destination
+  end
+
+  def create
+    @destination = Destination.new(destination_params)
+    if @destination.save
+      render json: @destination
+    else
+      render json: {error: 'Unable to create a new destination.'}
+    end
+  end
+
+  def update
+    if @destination.update(destination_params)
+      render json: @destination
+    else
+      render json: {error: 'Unable to edit the destination.'}
+    end
+  end
+
+  def destroy
+    @destination.destroy
+  end
+
+  private
+  def destination_params
+    params.require(:destination).permit(:name, :country, :image)
+  end
+
+  def set_destination
+    @destination = Destination.find(params[:id])
+  end
+
 end
