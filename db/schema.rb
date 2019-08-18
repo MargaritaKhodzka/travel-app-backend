b78253d17783a2ed47fc498201a8bae78cdf65b3
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-18 18:20:04 -0400

    added visited column to destinations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_001120) do
+ActiveRecord::Schema.define(version: 2019_08_18_220519) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_001120) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "visited", default: false
   end
 
 end
